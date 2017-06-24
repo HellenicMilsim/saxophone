1ce40204b5015e38d804c9e87a0fe9915564beac
Tony Klinakis <ak12536@my.bristol.ac.uk>
2017-06-25 00:55:45 +0100

    initial commit

---
+++ init.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * Responds to any HTTP request that can provide a "message" field in the body.
+ *
+ * @param {!Object} req Cloud Function request context.
+ * @param {!Object} res Cloud Function response context.
+ */
+exports.helloWorld = function helloWorld(req, res) {
+    console.log(req.body);
+    res.status(200);
+  }
+};
