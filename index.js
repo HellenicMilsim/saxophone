cb6aedba07d1a8302029b8baa576c925707ddbfc
Tony Klinakis <ak12536@my.bristol.ac.uk>
2017-06-25 01:00:08 +0100

    add keys skeleton

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
  * @param {!Object} res Cloud Function response context.
  */
 exports.helloWorld = function helloWorld(req, res) {
+	var api = require("./keys.json");
+	
     console.log(req.body);
     res.status(200);
   }
